b9ac6963426c1ade22079722113c258a3331def4
Leonardo Contreras <joseleoc123@gmail.com>
2024-06-25 23:22:10 -0400

    map view and lint project

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,54 +5,71 @@ module.exports = {
     jest: true,
   },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "prettier",
   ],
-  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
+  parser: "@typescript-eslint/parser",
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx"],
       extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
       parserOptions: {
-        project: ['./tsconfig.json'],
+        project: ["./tsconfig.json"],
       },
       rules: {
-        '@typescript-eslint/no-floating-promises': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-explicit-any': 'warn',
+        "@typescript-eslint/no-floating-promises": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-explicit-any": "warn",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
     ecmaFeatures: {
       jsx: true,
     },
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": [
+      "error",
+      {
+        arrowParens: "always",
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        jsxSingleQuote: false,
+        quoteProps: "as-needed",
+        semi: true,
+        printWidth: 100,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: "es5",
+      },
+      {
+        usePrettierrc: false,
+      },
+    ],
   },
 };
