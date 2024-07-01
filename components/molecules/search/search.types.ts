0247db0f43df6c67d25ad542839516f9703bfedb
Leonardo Contreras <joseleoc123@gmail.com>
2024-07-01 15:26:28 -0400

    folder structure

--- components/molecules/search/search.types.ts
+++ components/molecules/search/search.types.ts
@@ -1,5 +1,5 @@
 import { DropDownPickerProps } from 'react-native-dropdown-picker';
 
-export type SearchProps = DropDownPickerProps;
+export type SearchProps = DropDownPickerProps<any>;
 
 export type SearchTheme = 'LIGHT' | 'DARK';
