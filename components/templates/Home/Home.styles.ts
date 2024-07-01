b8a8e76a4da1869c7623a11d42338dc375b070f7
Leonardo Contreras <joseleoc123@gmail.com>
2024-07-01 14:59:53 -0400

    home list component

---
+++ components/templates/Home/Home.styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {},
+  item: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 10,
+    marginHorizontal: 20,
+  },
+  description: {
+    fontSize: 16,
+  },
+  counter: {
+    fontSize: 24,
+  },
+});
+
+export default styles;
