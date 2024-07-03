2bda3b7d650664ed1b5278b87b90d2f4a99d2f25
Raidenmystery <rafaeljoserivascalderon@gmail.com>
2024-07-03 18:50:18 -0400

    feat(Header): Add LoggedHeader Component

---
+++ components/organisms/LoggedHeader/LoggedHeader.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "@components/Themed";
+import { Image } from "react-native";
+import { styles } from "./LoggedHeader.styles";
+
+export default function LoggedHeader() {
+  return (
+    <View style={styles.loggedHeaderContainer}>
+      <Image source={require("@assets/images/avatar.png")} style={styles.image}></Image>
+      <View style={styles.infoContainer}>
+        <Text style={styles.companyText}>Advance Logistics</Text>
+        <Text style={styles.userText}>816 Frank Santiago</Text>
+        <Text style={styles.statusText}>Online</Text>
+      </View>
+    </View>
+  );
+}
