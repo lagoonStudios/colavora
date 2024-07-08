02aee96246c30888716a4d0aa67583886c8a95a3
Leonardo Contreras <joseleoc123@gmail.com>
2024-07-08 14:05:36 -0400

    login styles

--- components/atoms/Button/Button.styles.ts
+++ components/atoms/Button/Button.styles.ts
@@ -1,14 +1,32 @@
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  button: {
-    borderRadius: 8,
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  label: {
-    color: "#fff",
-  },
+    container: {
+        borderRadius: 8,
+    },
+    buttonShadow: Platform.select({
+        ios: {
+            shadowOffset: { width: 0, height: 4 },
+            shadowColor: "#000",
+            shadowOpacity: 0.25,
+            borderRadius: 8,
+        },
+        android: {
+            elevation: 3,
+            borderRadius: 8,
+        },
+        default: {},
+    }),
+    button: {
+        borderRadius: 8,
+        paddingVertical: 10,
+        paddingHorizontal: 24,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    label: {
+        color: "#fff",
+        fontWeight: "600",
+        fontSize: 18,
+    },
 });
