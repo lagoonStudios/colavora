b1d84fc55bbad3c4a9d86f755a42ff0a1222ea03
Leonardo Contreras <joseleoc123@gmail.com>
2024-07-11 15:57:18 -0400

    CA-3 Error handling

--- constants/Errors.ts
+++ constants/Errors.ts
@@ -1,5 +1,7 @@
 export enum ERROR_CODE {
     USER_NOT_CREATED = "USER_NOT_CREATED",
+    INVALID_GRANT = "invalid_grant",
     NO_TOKEN = "NO_TOKEN",
     INVALID_TOKEN = "INVALID_TOKEN",
+    UNKNOWN = "UNKNOWN"
 }
