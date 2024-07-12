888408aa8a36f61565d7f83b52cf293f9d16203c
Leonardo Contreras <joseleoc123@gmail.com>
2024-07-12 14:13:07 -0400

    inputs components

--- components/molecules/TextInput/TextInput.tsx
+++ components/molecules/TextInput/TextInput.tsx
@@ -1,68 +1,25 @@
 import React from "react";
-import { TextInput as DefaultTextInput, TextStyle } from "react-native";
 
-import { useThemeColor } from "@components/Themed";
-import { useColorScheme } from "@components/useColorScheme";
-import { Text, ThemeProps, View } from "@components/Themed";
-
-import { styles as defaultStyles } from "./TextInput.styles";
-
-export type TextInputProps = ThemeProps &
-  DefaultTextInput["props"] & {
-    label?: string;
-    labelStyle?: TextStyle;
-    errorText?: string;
-    errorStyle?: TextStyle;
-  };
+import { useFormContext } from "react-hook-form";
+import ControlledInput from "@molecules/ControlledInput";
+import { TextInputProps } from "@molecules/ControlledInput/ControlledInput";
 
 export default function TextInput(props: TextInputProps) {
-  const {
-    style,
-    lightColor,
-    darkColor,
-    label,
-    labelStyle,
-    errorText,
-    errorStyle,
-    ...otherProps
-  } = props;
-  const { default: backgroundColor } = useThemeColor(
-    { light: lightColor, dark: darkColor },
-    "background",
-  );
-  const { tint: textColor } = useThemeColor(
-    { light: lightColor, dark: darkColor },
-    "text",
-  );
-  const { tint: errorColor } = useThemeColor(
-    { light: lightColor, dark: darkColor },
-    "danger",
-  );
-  const theme = useColorScheme() ?? "light";
+  // --- Local state -----------------------------------------------------------
+  const { name } = props;
+  // --- END: Local state ------------------------------------------------------
 
-  return (
-    <View style={defaultStyles.container}>
-      {label && <Text style={[labelStyle, defaultStyles.label]}>{label}</Text>}
-      <View style={[defaultStyles.inputContainer, defaultStyles.inputShadow]}>
-        <DefaultTextInput
-          style={[
-            {
-              backgroundColor,
-              color: textColor,
-              borderColor: textColor,
-            },
-            defaultStyles.input,
-            style,
-          ]}
-          {...otherProps}
-          keyboardAppearance={theme}
-        />
-      </View>
-      {errorText && (
-        <Text style={[{ color: errorColor }, defaultStyles.error, errorStyle]}>
-          {errorText}
-        </Text>
-      )}
-    </View>
-  );
+  // --- Hooks -----------------------------------------------------------------
+  const formContext = useFormContext();
+  // --- END: Hooks ------------------------------------------------------------
+
+  if (!formContext || !name) {
+    const msg = !formContext
+      ? "TextInput must be wrapped by the FormProvider"
+      : "Name must be defined";
+    console.error(msg);
+    return null;
+  }
+
+  return <ControlledInput {...props} />;
 }
