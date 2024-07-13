d792ef9253c127dfaa0c5304097af1d70af56bec
Leonardo Contreras <joseleoc123@gmail.com>
2024-07-13 11:42:16 -0400

    lint

--- stores/AuthContext.ts
+++ stores/AuthContext.ts
@@ -1,16 +1,15 @@
 import { createContext } from "react";
 
 export interface AuthContextType {
-    token: string;
-    isLoggedIn: boolean;
-    saveToken: (token: string) => void;
-    logout: () => void;
+  token: string;
+  isLoggedIn: boolean;
+  saveToken: (token: string) => void;
+  logout: () => void;
 }
 
 export const AuthContext = createContext<AuthContextType>({
-    token: "",
-    isLoggedIn: false,
-    saveToken: (token: string) => { }
-    ,
-    logout: () => { },
+  token: "",
+  isLoggedIn: false,
+  saveToken: (token: string) => {},
+  logout: () => {},
 });
