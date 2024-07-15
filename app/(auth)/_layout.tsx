c2050663a6ff763cd73f138eb8833d53224635e2
Leonardo Contreras <joseleoc123@gmail.com>
2024-07-14 21:51:54 -0400

    fix home routes

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -12,8 +12,6 @@ export default function AuthLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
-      <Stack.Screen name="Home/index" />
-      <Stack.Screen name="ManifestsList/index" />
     </Stack>
   );
 }
