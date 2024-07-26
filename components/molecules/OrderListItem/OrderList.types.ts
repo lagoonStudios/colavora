7f883ac3a43c1a89cbc9c1248eef9f3fe4fd9260
Leonardo Contreras <joseleoc123@gmail.com>
2024-07-26 15:27:17 -0400

    lint

--- components/molecules/OrderListItem/OrderList.types.ts
+++ components/molecules/OrderListItem/OrderList.types.ts
@@ -1,14 +1,14 @@
 import { IFetchOrderData } from "@constants/types";
 
 export type TOrderListItemProps = Pick<
-    IFetchOrderData,
-    'shipmentID' |
-    'consigneeName' |
-    'senderName' |
-    'addressLine1' |
-    'addressLine2' |
-    'zip' |
-    'serviceTypeName' |
-    'referenceNo' |
-    'qty'
->
+  IFetchOrderData,
+  | "shipmentID"
+  | "consigneeName"
+  | "senderName"
+  | "addressLine1"
+  | "addressLine2"
+  | "zip"
+  | "serviceTypeName"
+  | "referenceNo"
+  | "qty"
+>;
--- components/templates/ShipmentDetail/ShipmentDetail.styles.ts
+++ components/templates/ShipmentDetail/ShipmentDetail.styles.ts
@@ -1,38 +1,37 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        gap: 24,
-        alignContent: "center",
-    },
-    infoContainer: {
-        flex: 1,
-        gap: 24,
-        alignContent: "center",
-        marginBottom: 24
-    },
-    mapContainer: {
-        height: "45%",
-        maxHeight: 500
-    },
-    section: {
-        gap: 6,
-        borderRadius: 13,
-        marginHorizontal: "auto",
-        width: "70%"
-    },
-    textHeader: {
-        fontWeight: 600,
-        fontSize: 16,
-    },
-    textSubtitle: {
-        fontWeight: 600,
-        fontSize: 14,
-    },
-    textBody: {
-        fontWeight: 600,
-        fontSize: 12,
-    }
-
+  container: {
+    flex: 1,
+    gap: 24,
+    alignContent: "center",
+  },
+  infoContainer: {
+    flex: 1,
+    gap: 24,
+    alignContent: "center",
+    marginBottom: 24,
+  },
+  mapContainer: {
+    height: "45%",
+    maxHeight: 500,
+  },
+  section: {
+    gap: 6,
+    borderRadius: 13,
+    marginHorizontal: "auto",
+    width: "70%",
+  },
+  textHeader: {
+    fontWeight: 600,
+    fontSize: 16,
+  },
+  textSubtitle: {
+    fontWeight: 600,
+    fontSize: 14,
+  },
+  textBody: {
+    fontWeight: 600,
+    fontSize: 12,
+  },
 });
