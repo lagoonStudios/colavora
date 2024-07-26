9d0f70e08fb2fb8ecdc9e9952a1dd22247858c79
Leonardo Contreras <joseleoc123@gmail.com>
2024-07-26 15:27:16 -0400

    orders list and PageHeader component

--- app/(auth)/ManifestsList/ManifestsList.styles.ts
+++ app/(auth)/ManifestsList/ManifestsList.styles.ts
@@ -3,10 +3,11 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  content: {
+    flex: 1,
     alignContent: "center",
     justifyContent: "center",
-    marginTop: 1,
     paddingHorizontal: 2,
-    height: "100%",
   },
 });
---
+++ components/molecules/PageHeader/PageHeader.styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    header: {
+        position: "relative",
+        alignItems: "center",
+        justifyContent: "center",
+        height: 110,
+    },
+    title: {
+        fontWeight: 700,
+        fontSize: 20,
+    },
+    arrowBack: {
+        position: "absolute",
+        left: 24,
+    }
+});
