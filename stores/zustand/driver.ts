5788f952466809b232e0220081e27778bdc8d542
Leonardo Contreras <joseleoc123@gmail.com>
2024-08-19 15:19:01 -0400

    feat: driver and company stores

---
+++ stores/zustand/driver.ts
@@ -0,0 +1,18 @@
+import { StateCreator } from "zustand";
+
+import { IFetchDriverData } from "@constants/types/general";
+
+export interface DriverSlice {
+    driver: IFetchDriverData | null;
+    addDriver: (driver: IFetchDriverData) => void;
+    resetDriver: () => void;
+}
+
+export const createDriverSlice: StateCreator<DriverSlice, [], []> = (set) => ({
+    driver: null,
+    addDriver: (newDriver: IFetchDriverData) => set(({ driver }) => {
+        driver = newDriver;
+        return { driver };
+    }),
+    resetDriver: () => set((state) => ({ ...state, driver: null })),
+});
