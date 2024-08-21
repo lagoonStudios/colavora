c1b453de33e41575d6e7985e529f54924e618c97
Leonardo Contreras <joseleoc123@gmail.com>
2024-08-21 13:05:00 -0400

    Search input local query

--- components/organisms/SearchInput/SearchInput.functions.tsx
+++ components/organisms/SearchInput/SearchInput.functions.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from "react-i18next";
 
 import { SearchItem, UseSearchDataProps } from "./SearchInput.types";
 import { debounce } from "@utils/index";
+import { searchShipments } from "@hooks/SQLite/shipments.local.queries";
 
 export const useSearchData = ({ text }: UseSearchDataProps) => {
   const [open, setOpen] = useState(false);
@@ -24,29 +25,21 @@ export const useSearchData = ({ text }: UseSearchDataProps) => {
         }
         setLoading(true);
         setOpen(true);
-        // Aquí va la lógica de tu búsqueda, por ejemplo, hacer una petición a una API
-        setTimeout(() => {
-          setData([
-            { label: "name 01", value: "123", href: "/ShipmentDetails" },
-            { label: "name 02", value: "124", href: "/ShipmentDetails" },
-            { label: "name 03", value: "125", href: "/ShipmentDetails" },
-            { label: "name 04", value: "126", href: "/ShipmentDetails" },
-            { label: "name 05", value: "127", href: "/ShipmentDetails" },
-            { label: "name 06", value: "128", href: "/ShipmentDetails" },
-            { label: "name 07", value: "129", href: "/ShipmentDetails" },
-            { label: "name 08", value: "130", href: "/ShipmentDetails" },
-            { label: "name 09", value: "131", href: "/ShipmentDetails" },
-            { label: "name 10", value: "132", href: "/ShipmentDetails" },
-            { label: "name 11", value: "133", href: "/ShipmentDetails" },
-            { label: "name 12", value: "134", href: "/ShipmentDetails" },
-            { label: "name 13", value: "135", href: "/ShipmentDetails" },
-            { label: "name 14", value: "136", href: "/ShipmentDetails" },
-            { label: "name 15", value: "137", href: "/ShipmentDetails" },
-            { label: "name 16", value: "138", href: "/ShipmentDetails" },
-            { label: "name 17", value: "139", href: "/ShipmentDetails" },
-          ]);
-          setLoading(false);
-        }, 2000);
+        searchShipments({ q: text })
+          .then((res) => {
+            const data = res.map((shipment) => ({
+              label: shipment.consigneeName || "",
+              value: String(shipment.shipmentID),
+            }));
+            setData(data);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error("🚀 ~ useSearchData ~ error:", error);
+            setError(error.message);
+            Toast.show(t("ERRORS.UNKNOWN"));
+            setLoading(false);
+          });
       } catch (error) {
         console.error("🚀 ~ useSearchData ~ error:", error);
         setError("Error");
