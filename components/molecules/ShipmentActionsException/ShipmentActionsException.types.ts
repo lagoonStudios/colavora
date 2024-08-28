a73b9de35cef5aa1edaa4308a0ec40429873a29f
Leonardo Contreras <joseleoc123@gmail.com>
2024-08-28 18:36:15 -0400

    feat: retorno de acciones

--- components/molecules/ShipmentActionsException/ShipmentActionsException.types.ts
+++ components/molecules/ShipmentActionsException/ShipmentActionsException.types.ts
@@ -1,3 +1,4 @@
+import { ShipmentActionsButtonItem } from "@organisms/ShipmentActions/ShipmentAction.constants";
 import { ShipmentDetailsTabsItem } from "@templates/ShipmentDetailsTabs/ShipmentDetailsTabs.constants";
 import { ImagePickerAsset } from "expo-image-picker";
 
@@ -9,4 +10,5 @@ export interface IOrderExceptionForm {
 
 export interface IShipmentActionsException {
   setSelectedTab: React.Dispatch<React.SetStateAction<ShipmentDetailsTabsItem>>;
+  setOption: (option: ShipmentActionsButtonItem) => void
 }
