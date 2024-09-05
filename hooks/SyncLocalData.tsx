80a624dd23e95c267f67629bea2d49c7f5c05674
Leonardo Contreras <joseleoc123@gmail.com>
2024-09-05 00:51:57 -0400

    feat(sync): Sync data periodically

--- hooks/SyncLocalData.tsx
+++ hooks/SyncLocalData.tsx
@@ -1,19 +1,88 @@
 import { useStore } from "@stores/zustand";
 import { useEffect } from "react";
+import { getAllShipmentIds, resetDatabase } from "./SQLite";
+import { useTranslation } from "react-i18next";
+import Toast from "react-native-root-toast";
+import { useIsConnected } from "react-native-offline";
 
 /** Gets the sync period (in minutes) from the store and sets up a timer to sync the data every syncPeriod minutes */
 export const useSyncDataByPeriod = () => {
   // --- Hooks -----------------------------------------------------------------
-  const { syncPeriod, setSyncing, user } = useStore();
+  const {
+    syncPeriod,
+    setSyncing,
+    isSyncing,
+    user,
+    setModal,
+    setLastSyncDate,
+    addManifestIds,
+    addManifestId,
+    addShipmentIds,
+    setVisible,
+  } = useStore();
+  const { t } = useTranslation();
+  const isConnected = useIsConnected();
+
   // --- END: Hooks ------------------------------------------------------------
 
   // --- Side effects ----------------------------------------------------------
   useEffect(() => {
-    setSyncing(true);
+    console.log("Syncing data1", new Date().getSeconds());
+
     const id = setInterval(
       () => {
-        setSyncing(false);
-        console.info("Implement sync logic here", new Date().getSeconds());
+        if (!isConnected) {
+          Toast.show(t("NETWORK_ERROR.TITLE"));
+          return;
+        }
+        console.log("Syncing data", new Date().toISOString());
+        if (user && !isSyncing) {
+          setSyncing(true);
+          resetDatabase(user, { t, setModalMessage: setModal })
+            .then((values) => {
+              setLastSyncDate(new Date().toISOString());
+              const manifestIdsFromFetching = values.manifests.map(
+                ({ manifest }) => Number(manifest)
+              );
+
+              if (manifestIdsFromFetching.length > 0) {
+                addManifestIds(
+                  values.manifests.map(({ manifest }) => Number(manifest))
+                );
+                const firstManifest = manifestIdsFromFetching.sort(
+                  (a, b) => a - b
+                )?.[0];
+
+                if (firstManifest) {
+                  addManifestId(String(firstManifest));
+                  getAllShipmentIds({ manifestID: String(firstManifest) }).then(
+                    (shipmentsIdsLocal) => {
+                      const shipmentIds = shipmentsIdsLocal?.map(
+                        ({ shipmentID }) => shipmentID
+                      );
+
+                      if (shipmentIds?.length > 0) addShipmentIds(shipmentIds);
+                    }
+                  );
+                }
+              }
+
+              setSyncing(false);
+              setVisible(false);
+              Toast.show(t("TOAST.SYNC_SUCCESS"));
+            })
+            .catch((error) => {
+              console.error(
+                "🚀 ~ file: SyncLocalData.tsx:63 ~ useSyncDataByPeriod ~ error:",
+                error
+              );
+              setSyncing(false);
+              setVisible(false);
+              Toast.show(t("TOAST.SYNC_FAIL"));
+            });
+          setSyncing(false);
+          console.info("Implement sync logic here", new Date().getSeconds());
+        }
       },
       syncPeriod * 60 * 1000
     );
