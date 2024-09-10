e1ae2d2785b21ffe620995048eff3afe7a3873a4
Raidenmystery <rafaeljoserivascalderon@gmail.com>
2024-09-10 08:47:51 -0400

    fix(general): fix complete order and comment parsing

--- app/(auth)/ShipmentDetails/ShipmentDetails.functions.tsx
+++ app/(auth)/ShipmentDetails/ShipmentDetails.functions.tsx
@@ -1,10 +1,11 @@
 import { IFetchShipmentByIdData } from "@constants/types/shipments";
+import { getPiecesByShipmentID } from "@hooks/SQLite/queries/pieces.local.queries";
 import { useStore } from "@stores/zustand";
 import { useEffect, useState } from "react";
 
 export const useShipmentDetailsData = () => {
   // --- Hooks -----------------------------------------------------------------
-  const { shipment } = useStore();
+  const { shipment, addPieces } = useStore();
   // --- END: Hooks ------------------------------------------------------------
 
   // --- Local state -----------------------------------------------------------------
@@ -14,6 +15,14 @@ export const useShipmentDetailsData = () => {
   // --- END: Local state ------------------------------------------------------------
 
   // --- Side effects ----------------------------------------------------------
+  useEffect(() => {
+    if (shipment.shipmentID) {
+      getPiecesByShipmentID({ shipmentID: shipment.shipmentID }).then((values) => {
+        addPieces(values)
+      })
+    }
+  }, [shipment.shipmentID]);
+
   useEffect(() => {
     if (shipment == undefined) {
       setError(new Error("No shipment ID provided"));
