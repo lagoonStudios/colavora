399dfe2926a7ba74448cd5f395e5638b2def8fe9
Raidenmystery <rafaeljoserivascalderon@gmail.com>
2024-09-14 13:34:21 -0400

    feat(general): add new optional param for fetching data date

--- constants/url.ts
+++ constants/url.ts
@@ -1,4 +1,9 @@
 export const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL ?? "";
 export const API_KEY = process.env.EXPO_PUBLIC_API_KEY ?? "";
 export const AUTH0_CLIENT_ID = process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID ?? "";
-export const AUTH0_DOMAIN = process.env.EXPO_PUBLIC_AUTH0_DOMAIN ? "https://" + process.env.EXPO_PUBLIC_AUTH0_DOMAIN : "";
+export const TEST_TIME = process.env.EXPO_PUBLIC_TEST_TIME
+  ? Number(process.env.EXPO_PUBLIC_TEST_TIME)
+  : 0;
+export const AUTH0_DOMAIN = process.env.EXPO_PUBLIC_AUTH0_DOMAIN
+  ? "https://" + process.env.EXPO_PUBLIC_AUTH0_DOMAIN
+  : "";
