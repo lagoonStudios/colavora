9af871da79aa90a24bb94500f53cedab620fafba
Leonardo Contreras <joseleoc123@gmail.com>
2024-09-13 22:40:59 -0400

    feat(user): enhance user data fetching with error handling and add translation for fetching user error messages

--- hooks/syncData/user.ts
+++ hooks/syncData/user.ts
@@ -1,4 +1,8 @@
-import { useAuth0UserInfoData, useDriverDataByAuth0, useUserData } from "@hooks/queries";
+import {
+  useAuth0UserInfoData,
+  useDriverDataByAuth0,
+  useUserData,
+} from "@hooks/queries";
 import { useState, useEffect } from "react";
 import { useStore } from "@stores/zustand";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -18,10 +22,13 @@ export function useDriverFetch() {
   const { t } = useTranslation();
   const isConnected = useIsConnected();
 
-  const { user, addUser, setModal } = useStore();
+  const { user, addUser, setModal, setVisible, isSyncing } = useStore();
   const { data: userData } = useUserData(userId);
-  const { data: users } = useDriverDataByAuth0({ companyID: user?.companyID, authId: auth0Id });
-  const { data: userInfo } = useAuth0UserInfoData(userEmail);
+  const { data: users, error: usersError } = useDriverDataByAuth0({
+    companyID: user?.companyID,
+    authId: auth0Id,
+  });
+  const { data: userInfo, error: userInfoError } = useAuth0UserInfoData(userEmail);
   // --- END: Hooks ------------------------------------------------------------
 
   // --- Side effects ----------------------------------------------------------
@@ -30,41 +37,46 @@ export function useDriverFetch() {
       try {
         const jsonValue = await AsyncStorage.getItem("auth0:email");
         if (jsonValue != null) {
-          blockAction(true)
+          blockAction(true);
           setUserEmail(jsonValue);
         }
-      } catch (e) { }
+      } catch (e) {
+        console.error("🚀 ~ file: user.ts:44 ~ getData ~ e:", e);
+
+      }
     };
 
     const getLocalData = async () => {
       try {
         const jsonValue = await AsyncStorage.getItem("auth0:user");
         if (jsonValue != null) {
-          blockAction(true)
-          const localUser = JSON.parse(jsonValue) as IFetchUserData
-          addUser(localUser)
+          blockAction(true);
+          const localUser = JSON.parse(jsonValue) as IFetchUserData;
+          addUser(localUser);
         }
-      } catch (e) { }
-    }
+      } catch (e) {
+        console.error("🚀 ~ file: user.ts:58 ~ getLocalData ~ e:", e);
+
+      }
+    };
 
     if (setOnce === false)
       if (Boolean(isConnected) && user === null) getData();
       else if (user === null) getLocalData();
-  }, [setOnce])
+  }, [setOnce]);
 
   useEffect(() => {
     if (isConnected)
       if (userInfo && userInfo.sub) {
-        setModal(t("MODAL.FETCHING_USER"))
-        setAuth0Id(userInfo?.sub)
+        setModal(t("MODAL.FETCHING_USER"));
+        setAuth0Id(userInfo?.sub);
       }
-  }, [isConnected, userInfo])
+  }, [isConnected, userInfo]);
+
 
   useEffect(() => {
-    if (isConnected)
-      if (users && users?.length !== 0)
-        setUserId(users?.[0])
-  }, [isConnected, users])
+    if (isConnected) if (users && users?.length !== 0) setUserId(users?.[0]);
+  }, [isConnected, users]);
 
   useEffect(() => {
     if (isConnected)
@@ -74,15 +86,21 @@ export function useDriverFetch() {
           driverName: userInfo?.name,
           driverID: userData?.userID,
           logo: userInfo?.picture,
-        }
-        addUser(newUser)
+        };
+        addUser(newUser);
       }
   }, [userData, userInfo]);
 
-  useEffect(() => {
-    if (user !== null)
-      AsyncStorage.setItem("auth0:user", JSON.stringify(user));
 
+  useEffect(() => {
+    console.error({ userInfoError, usersError });
+    if (userInfoError || usersError) {
+      setModal(t("MODAL.FETCHING_USER_ERROR"))
+    };
+  }, [userInfoError, usersError]);
+
+  useEffect(() => {
+    if (user !== null) AsyncStorage.setItem("auth0:user", JSON.stringify(user));
   }, [user]);
   // --- END: Side effects -----------------------------------------------------
 
