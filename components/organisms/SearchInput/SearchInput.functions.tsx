998d79675809221846be4743ab99431cd50aaeae
RafaelRivas-PT <rafaelr@prettytechnical.io>
2025-06-13 11:38:44 -0400

    refactor(shipments.local.queries): add new logic for search

--- components/organisms/SearchInput/SearchInput.functions.tsx
+++ components/organisms/SearchInput/SearchInput.functions.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 
 import { SearchItem, UseSearchDataProps } from "./SearchInput.types";
 import { debounce } from "@utils/index";
-import { searchShipments } from "@hooks/SQLite";
+import { searchShipmentsUnified } from "@hooks/SQLite";
 
 export const useSearchData = ({ text }: UseSearchDataProps) => {
   const [open, setOpen] = useState(false);
@@ -25,7 +25,7 @@ export const useSearchData = ({ text }: UseSearchDataProps) => {
         }
         setLoading(true);
         setOpen(true);
-        searchShipments({ q: text })
+        searchShipmentsUnified({ text })
           .then((res) => {
             const data = res.map((shipment) => ({
               label: shipment.consigneeName || "",
