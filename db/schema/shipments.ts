12bde2423f5e5027926761778f291ebe9726578a
Leonardo <joseleoc123@gmail.com>
2025-06-21 13:35:27 -0400

    remove unused imports

--- db/schema/shipments.ts
+++ db/schema/shipments.ts
@@ -2,58 +2,72 @@ import { sqliteTable, text, integer, real } from "drizzle-orm/sqlite-core";
 import { syncColumns } from "./base";
 
 export const shipmentsTable = sqliteTable("shipments", {
-  shipmentID: integer().primaryKey({ autoIncrement: true }),
-  companyID: text().notNull(),
-  waybill: text(),
-  serviceType: integer(),
-  serviceTypeName: text(),
-  packageType: integer(),
-  readyDate: text(),
-  dueDate: text(),
-  codType: text(),
-  codAmount: real(),
-  sender: text(),
-  senderName: text(),
-  senderAddressLine1: text(),
-  senderAddressLine2: text(),
-  senderZip: text(),
-  senderPhoneNumber: text(),
-  senderContactPerson: text(),
-  orderNotes: text(),
-  consigneeNum: text(),
-  consigneeName: text(),
   addressLine1: text(),
   addressLine2: text(),
-  zip: text(),
-  phoneNumber: text(),
-  contactPerson: text(),
-  createdUserID: integer(),
-  createdDate: text(),
-  lastTransferDate: text(),
-  status: text().notNull(),
-  qty: integer(),
-  items: text(),
-  templateID: integer(),
-  manifestDL: text(),
-  assignPK: integer(),
   assignDL: integer(),
-  division: text(),
-  lastEventComment: text(),
-  reason: text(),
+  assignPK: integer(),
   barcode: text(),
-  referenceNo: text(),
-  manifestPk: text(),
-  manifest: text().notNull(),
+  city: text(),
+  codAmount: real(),
+  codType: text(),
+  companyID: text().notNull(),
+  consigneeName: text(),
+  consigneeNum: text(),
+  contactPerson: text(),
+  createdDate: text(),
+  createdUserID: integer(),
+  division: text(),
+  driverAssign: integer(),
+  dueDate: text(),
+  items: text(),
+  lastEventComment: text(),
+  lastTransferDate: text(),
   latitude: real(),
   longitude: real(),
-  city: text(),
-  photoOnEvent: integer({ mode: "boolean" }),
+  manifest: text().notNull(),
+  manifestDL: text(),
+  manifestPk: text(),
+  orderNotes: text(),
+  packageType: integer(),
+  phoneNumber: text(),
   photoOnDelivery: integer({ mode: "boolean" }),
-  driverAssign: integer(),
+  photoOnEvent: integer({ mode: "boolean" }),
+  qty: integer(),
+  readyDate: text(),
+  reason: text(),
+  referenceNo: text(),
+  sender: text(),
+  senderAddressLine1: text(),
+  senderAddressLine2: text(),
+  senderContactPerson: text(),
+  senderName: text(),
+  senderPhoneNumber: text(),
+  senderZip: text(),
+  serviceType: integer(),
+  serviceTypeName: text(),
+  shipmentID: integer().primaryKey({ autoIncrement: true }),
   signatureOnDelivery: integer({ mode: "boolean" }),
-
+  status: text().notNull(),
+  templateID: integer(),
+  waybill: text(),
+  zip: text(),
   ...syncColumns,
 });
 
 export type TShipmentsData = typeof shipmentsTable.$inferSelect;
 export type TShipmentInsertData = Omit<TShipmentsData, "isSync" | "lastSync">;
+
+export type TShipmentListData = Pick<
+  TShipmentsData,
+  | "shipmentID"
+  | "consigneeName"
+  | "zip"
+  | "senderName"
+  | "serviceTypeName"
+  | "addressLine1"
+  | "addressLine2"
+  | "referenceNo"
+  | "dueDate"
+  | "qty"
+  | "city"
+>;
